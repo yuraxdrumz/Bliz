8a9ac42958c8f88d2bfceced48afa64f24393627
yura <yuri.khomyakov@ironsrc.com>
2018-05-15 14:30:51 +0300

    added ETag

--- src/response.js
+++ src/response.js
@@ -1,10 +1,13 @@
 import http from 'http'
+import etag from 'etag'
 
 const res = Object.create(http.ServerResponse.prototype)
 
 res.json = function(data){
   this.setHeader('Content-Type','application/json')
-  this.end(JSON.stringify(data, null, 3))
+  const stringified = JSON.stringify(data, null, 3)
+  this.setHeader('ETag', etag(stringified))
+  this.end(stringified)
 }
 
 export default res
